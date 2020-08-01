655c8cca96df5e77e273172736cf3af354c24a8d
C.J Levey <cameron.leverett@yahoo.com>
2020-07-31 20:52:48 -0700

    won't need this stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,20 +3,7 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def new
-    @categories.new
-  end
-
-  def create
-    if current_user.admin = true
-      @categories = Category.find_or_create_by(name: params[:name])
-    end
-  end
-
   def show
     @category = Category.find_by(id: params[:id])
-  end
-
-  def destroy
   end
 end
