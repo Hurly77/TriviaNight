07e074e9fb9f35beff848ac66d371c2ee2661cd2
C.J Levey <cameron.leverett@yahoo.com>
2020-07-31 20:59:32 -0700

    useing api class to seed data

---
+++ app/models/api.rb
@@ -0,0 +1,49 @@
+require 'open-uri'
+require 'json'
+class Api
+
+  def self.urls
+    urls = [
+      "https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=10&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=12&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=13&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=14&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=15&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=16&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=17&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=19&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=20&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=21&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=22&difficulty=easy&type=multiple",
+      "https://opentdb.com/api.php?amount=20&category=23&difficulty=easy&type=multiple"
+    ]
+  end
+
+  def self.urls_parse
+    nest = urls.map {|url| JSON.parse(open(url).read)["results"]}.flatten
+    nest.map {|hash| hash.delete("type")} 
+    return nest
+  end
+
+  def self.create_category
+    @data = self.urls_parse
+    categories = @data.map {|hash| hash["category"]}.uniq
+    categories.each {|name| Category.find_or_create_by(name: name)}
+  end
+
+  def self.category_ids
+      @data.map do |hash| category = Category.find_by(name: hash["category"])
+      hash["category_id"] = category.id 
+    end
+      @data.each {|hash| hash.delete("category")}
+  end
+
+  def self.create_questions
+    self.create_category
+    self.category_ids
+    @data.each {|hash| Question.create(hash)}
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #triviagames
-require 'open-uri'
-TriviaGame.create(name: "Film")
 #likes
 #users
 #games
 #categories
-Category.create(name: "Entertainment: Film")
 #Questions
 #easy
-def create_category
-  categories.each do |name|
-    category = Category.new(name: name)
-    category.save
-  end
-end
+Api.create_questions
 #medium
 #hard
 #rounds
- create_table "questions", force: :cascade do |t|
-    t.string "question"
-    t.string "difficulty"
-    t.string "correct_answer"
-    t.string "incorrect_answers"
-    t.integer "points"
-    t.integer "round_id"
-    t.integer "category_id"
-    t.integer "game_id"
-    t.integer "user_id"
-  end
-  grades = Hash.new
-grades["Dorothy Doe"] = 9
