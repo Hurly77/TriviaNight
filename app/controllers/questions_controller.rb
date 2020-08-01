8d3d8591714a2e2ea84765cc95b671f3d3f39175
C.J Levey <cameron.leverett@yahoo.com>
2020-07-31 20:55:04 -0700

    don't need used api url to seed data

--- app/controllers/questions_controller.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-class QuestionsController < ApplicationController
-  
-  def request_api
-    url = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple"
-    data = JSON.parse(open(url).read)
-  end
-end
