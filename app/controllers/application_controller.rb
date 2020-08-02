41920a402648ee91a904e526d3912661855bd8e1
C.J Levey <cameron.leverett@yahoo.com>
2020-08-02 16:15:30 -0700

    don't made helper for my game controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :alert, :msg, :error
   before_action :config_permit_params, if: :devise_controller?
+  helper_method :current_game
+  helper_method :current_question
 
   def home
     if current_user.nil?
       redirect_to new_user_registration_path
     end
+  end
+
+  def current_game
+    @game = Game.find_by(id: params[:game_id])
   end
 
   protected
