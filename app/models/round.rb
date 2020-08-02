c0e569cbbf864d2df1b6c5cf57bf09449b96da9e
C.J Levey <cameron.leverett@yahoo.com>
2020-08-02 16:23:30 -0700

    adding more functionality to app

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,14 @@ class Round < ApplicationRecord
   belongs_to :game
   belongs_to :user
   belongs_to :question
+
+    def correct?(question)
+      @q = Question.find_by(id: self.question_id)
+      if @q.correct_answer == self.question
+        current_user.points + @q.points
+      "Yes That is correct, you earned #{@q.points}"
+      else
+     "Sorry, that was wrong, the correct answer is #{@q.correct_answer}"
+    end
+  end
 end
