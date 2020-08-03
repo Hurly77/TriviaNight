b61d9a23c8c136b5461e6bb7f4e206f542a5beec
C.J Levey <cameron.leverett@yahoo.com>
2020-08-02 20:17:42 -0700

    wasn't working now it is

--- app/models/round.rb
+++ app/models/round.rb
@@ -11,11 +11,13 @@ class Round < ApplicationRecord
       @user = User.find_by(id: self.user_id)
     @q = Question.find_by(id: self.question_id)
     if @q.correct_answer == self.user_answer
-      @user.points + @q.points
+      @user.points = @user.points + @q.points
+      @user.save
     "Yes that is correct, you earned #{@q.points}"
     else
-      @user.points - @q.points
-   "Sorry, that was wrong, the correct answer is #{@q.correct_answer}"
+      @user.points = @user.points - @q.points
+      @user.save
+   "Sorry, that was wrong, the correct answer is #{@q.correct_answer}, your answer was #{self.user_answer}"
   end
 end
 end
