e2e57bf62cddb3fa0429bf4d225b308b58055706
C.J Levey <cameron.leverett@yahoo.com>
2020-08-02 19:16:53 -0700

    usless code

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,8 +4,4 @@ class Game < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  def self.category_id(id)
-    Game.find_by(id: id).category_id
-  end
-  
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,13 +3,19 @@ class Round < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-    def correct?(question)
-      @q = Question.find_by(id: self.question_id)
-      if @q.correct_answer == self.question
-        current_user.points + @q.points
-      "Yes That is correct, you earned #{@q.points}"
-      else
-     "Sorry, that was wrong, the correct answer is #{@q.correct_answer}"
-    end
+    def self.category_id(id)
+    Game.find_by(id: id).category_id
   end
+  
+  def correct?(answer)
+      @user = User.find_by(id: self.user_id)
+    @q = Question.find_by(id: self.question_id)
+    if @q.correct_answer == self.user_answer
+      @user.points + @q.points
+    "Yes that is correct, you earned #{@q.points}"
+    else
+      @user.points - @q.points
+   "Sorry, that was wrong, the correct answer is #{@q.correct_answer}"
+  end
+end
 end
